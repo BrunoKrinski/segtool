aa9ed9ab2c43fd4abb73a3da779753cdf1b1326b
Bruno Alexandre Krinski <brunokrinski@outlook.com>
2021-01-07 23:46:59 -0300

    update

---
+++ cihp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#SBATCH -t 7-00:00:00
+#SBATCH -c 1
+#SBATCH -o /home/bakrinski/segtool/cihp_log.out
+#SBATCH --job-name=segtool_train
+#SBATCH -n 1 #NUM_DE_PROCESSOS
+#SBATCH -p 7d
+#SBATCH -N 1 #NUM_NODOS_NECESSARIOS
+#SBATCH --nodelist=vti2-ib
+#SBATCH --gres=gpu:1
+#SBATCH -e /home/bakrinski/segtool/cihp_error.out
+
+export PATH="/home/bakrinski/anaconda3/bin:$PATH"
+
+module load libraries/cuda/10.1
+
+srun python main.py --configs configs/resnet50_unet_cihp_fold0.yml
+srun python main.py --configs configs/resnet50_unet_cihp_fold1.yml
+srun python main.py --configs configs/resnet50_unet_cihp_fold2.yml
+srun python main.py --configs configs/resnet50_unet_cihp_fold3.yml
+srun python main.py --configs configs/resnet50_unet_cihp_fold4.yml
