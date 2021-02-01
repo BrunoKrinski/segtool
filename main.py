5e5bc445de5dbd53d2814052f12faedda4b0fb07
Bruno Alexandre Krinski <brunokrinski@outlook.com>
2021-02-01 19:06:42 -0300

    update

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def get_training_augmentation(height=256, width=256):
         albu.LongestMaxSize(max_size, interpolation=cv2.INTER_NEAREST, p=1),
         albu.PadIfNeeded(min_height=height, min_width=width, always_apply=True, border_mode=cv2.BORDER_CONSTANT, value=0, mask_value=0),
         #albu.RandomBrightnessContrast(p=0.5),
-        albu.CoarseDropout(p=0.5, max_holes=100),
+        #albu.CoarseDropout(p=0.5, max_holes=100),
         #albu.CLAHE(p=0.5, tile_grid_size=(64, 64)),
         #albu.HorizontalFlip(p=0.5),
 
