5474b891b72f351aa3a99d634c6b15982f85f18c
Bruno Alexandre Krinski <brunokrinski@outlook.com>
2021-02-16 20:33:01 -0300

    update

--- train_mosmed.sh
+++ train_mosmed.sh
@@ -6,7 +6,7 @@
 #SBATCH -n 1 #NUM_DE_PROCESSOS
 #SBATCH -p 7d
 #SBATCH -N 1 #NUM_NODOS_NECESSARIOS
-#SBATCH --nodelist=vti2-ib
+#SBATCH --nodelist=pti
 #SBATCH --gres=gpu:1
 #SBATCH -e /home/bakrinski/segtool/logs/mosmed_error.out
 
