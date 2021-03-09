bfb0ff0e6bc4e0daa38b79945a7a49edfd34227c
Bruno Alexandre Krinski <brunokrinski@outlook.com>
2021-03-09 18:05:25 -0300

    update

--- gen_eval_configs.py
+++ gen_eval_configs.py
@@ -116,7 +116,7 @@ encoders = ['resnet18',
 
 encoders = ['resnet50']
 nodes = ['vti1-ib', 'pti', 'vti2-ib']
-decoders = ['unet','fpn','pspnet','linknet', 'unetplusplus', 'pan', 'manet', 'deeplabv3', 'deeplabv3plus']
+decoders = ['unetplusplus','unet','fpn','pspnet','linknet', 'pan', 'manet', 'deeplabv3', 'deeplabv3plus']
 #datasets = ['ricord1a', 'medseg', 'covid20cases', 'mosmed', 'covid19china']
 datasets = ['medseg', 'mosmed', 'covid19china']
 
